b38ef90c95f973eb7bcd39372c7fd1eebeda3e87
Alejandro <abg_blanco@hotmail.com>
2025-05-22 17:58:15 +0200

    seeder arreglado

--- database/seeders/BoardSeeder.php
+++ database/seeders/BoardSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 use App\Models\Board;   
 use App\Models\TaskList;
 use App\Models\Task;
@@ -11,10 +12,16 @@ class BoardSeeder extends Seeder
 {
     public function run()
     {
-        Board::factory()->count(3)->create()->each(function ($board) {
-            $board->lists()->saveMany(TaskList::factory()->count(4)->make())->each(function ($lista) {
-                $lista->tasks()->saveMany(Task::factory()->count(3)->make());
-            });
-        });
+        $user = User::first();
+        Board::factory()
+            ->count(3)
+            ->for($user)
+            ->has(
+                TaskList::factory()
+                    ->count(3)
+                    ->has(Task::factory()->count(5), 'tasks'),
+                'lists'
+            )
+            ->create();
     }
 }
