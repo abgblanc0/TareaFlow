90099f21ce3349eaf9af7ce7dfd263eb2bc85829
Alejandro <abg_blanco@hotmail.com>
2025-05-25 19:37:06 +0200

    cambios menores

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -17,8 +17,8 @@ import AppLogoIcon from './app-logo-icon';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
-        href: '/dashboard',
+        title: 'Mis tableros',
+        href: '/boards',
         icon: LayoutGrid,
     },
 ];
@@ -26,13 +26,8 @@ const mainNavItems: NavItem[] = [
 const rightNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/abgblanc0/TareaFlow',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
     },
 ];
 
