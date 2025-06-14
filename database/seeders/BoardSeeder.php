65a4363ed90d48e032771a7d67f4b0552277cea4
Alejandro <abg_blanco@hotmail.com>
2025-06-14 14:23:37 +0200

    updt seeder para posiciones

--- database/seeders/BoardSeeder.php
+++ database/seeders/BoardSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Board;   
+use App\Models\Board;
 use App\Models\TaskList;
 use App\Models\Task;
 
@@ -13,13 +14,25 @@ class BoardSeeder extends Seeder
     public function run()
     {
         $user = User::first();
+
         Board::factory()
             ->count(3)
             ->for($user)
             ->has(
                 TaskList::factory()
                     ->count(3)
-                    ->has(Task::factory()->count(5), 'tasks'),
+                    ->has(
+                        Task::factory()
+                            ->count(5)
+                            ->state(new Sequence(
+                                ['position' => 0],
+                                ['position' => 1],
+                                ['position' => 2],
+                                ['position' => 3],
+                                ['position' => 4],
+                            )),
+                        'tasks'
+                    ),
                 'lists'
             )
             ->create();
