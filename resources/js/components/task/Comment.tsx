2bfa1ed0b028e1d232dc389478bd1e8c2c7e361d
Alejandro <abg_blanco@hotmail.com>
2025-06-15 16:26:27 +0200

    no hace falta

--- resources/js/components/task/Comment.tsx
+++
@@ -1,9 +0,0 @@
-import { CommentT } from '@/types';
-export default function Comment(comment: CommentT) {
-  return (
-    <div className='flex flex-col'>
-      <div><h2>{comment.user.name}</h2><h2>{comment.created_at}</h2></div>
-      <div><p>{comment.content}</p></div>
-    </div>
-  );
-}
