0a34dbd52ddbe0a654a7da7b2997c28c150b20b3
Alejandro <abg_blanco@hotmail.com>
2025-06-15 12:23:45 +0200

    comments 0.1

---
+++ resources/js/components/Comments.tsx
@@ -0,0 +1,8 @@
+export default function Comments() {
+  return (
+    <div className="w-1/2 flex flex-col">
+      <div className="p-2 border-b-2">Comentarios</div>
+      <div>adsadasd</div>
+    </div>
+  );
+}
